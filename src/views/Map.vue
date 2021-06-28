098bd3a646bcb63f306c1a7c3b6582277b48f4ba
baronbunny <its@baronbunny.cn>
2021-06-28 14:37:49 +0800

    加入关于页面

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -25,35 +25,39 @@
           position="bottomleft"
           :prefix="attributionPrefix"
         ></l-control-attribution>
+        <l-control-attribution
+          position="topleft"
+          :prefix="status"
+        ></l-control-attribution>
         <l-control-zoom position="bottomright"></l-control-zoom>
         <l-marker :lat-lng="center">
           <l-icon :icon-url="iconUrl" :icon-size="[40, 40]"> </l-icon>
           <l-popup>
             中国空间站天和核心舱
             <hr />
-            高度：{{ height.toFixed(2) }} km<br />
-            速度：{{ velocity.toFixed(2) }} km/s<br />
-            位置：{{ lat.toFixed(2) }}&deg; N {{ lng.toFixed(2) }}&deg; E
+            海拔：{{ height }} km<br />
+            速度：{{ velocity }} km/s<br />
+            位置：{{ lat }}&deg; N {{ lng }}&deg; E
           </l-popup>
         </l-marker>
         <l-polyline :lat-lngs="tracks"></l-polyline>
         <l-circle
           :lat-lng="center"
-          :radius="1000e3"
+          :radius="1000000"
           :color="'#c22'"
           :opacity="3"
           :weight="1"
         />
         <l-circle
           :lat-lng="center"
-          :radius="600e3"
+          :radius="600000"
           :color="'#c22'"
           :opacity="3"
           :weight="1"
         />
         <l-circle
           :lat-lng="center"
-          :radius="200e3"
+          :radius="300000"
           :color="'#c22'"
           :opacity="3"
           :weight="1"
@@ -69,6 +73,7 @@ import axios from 'axios'
 import {
   getGroundTracks,
   getSatelliteInfo,
+  getSatelliteName,
   parseTLE,
   SatelliteInfoOutput,
   ThreeGroundTracks,
@@ -110,6 +115,7 @@ export default defineComponent({
       map: {} as LeafletMap,
       tle: {} as TLE,
       tracks: [[], [], []] as ThreeGroundTracks,
+      satName: '',
       satInfo: {} as SatelliteInfoOutput,
       satMarker: null,
       TLEUrl: 'https://www.positiveoutlook.cn/satcat/tle/48274',
@@ -131,24 +137,27 @@ export default defineComponent({
         }
       ],
       attributionPrefix:
-        '<a href="./about">关于此应用</a> | 基础地图和数据来自 OpenStreetMap'
+        '<a href="./about">关于此应用</a> | <a href="https://www.positiveoutlook.cn/">回到主页 🏠</a> | 基础地图来自 OpenStreetMap'
     }
   },
   computed: {
     height(): number {
-      return this.satInfo.height || 0
+      return Number(Number(this.satInfo.height).toFixed(2)) || 0
     },
     velocity(): number {
-      return this.satInfo.velocity || 0
+      return Number(Number(this.satInfo.velocity).toFixed(2)) || 0
     },
     lat(): number {
-      return this.satInfo.lat || 0
+      return Number(Number(this.satInfo.lat).toFixed(2)) || 0
     },
     lng(): number {
-      return this.satInfo.lng || 0
+      return Number(Number(this.satInfo.lng).toFixed(2)) || 0
     },
     center(): [number, number] {
       return [this.lat, this.lng]
+    },
+    status(): string {
+      return `${this.satName} | 海拔: ${this.height} km 速度: ${this.velocity} km/s 位置: ${this.lat}&deg; N ${this.lng}&deg; E`
     }
   },
   watch: {
@@ -164,6 +173,7 @@ export default defineComponent({
     // 获取 TLE
     axios.get(this.TLEUrl).then((resp) => {
       this.tle = parseTLE(resp.data.tle)
+      this.satName = getSatelliteName(this.tle)
       this.satInfo = getSatelliteInfo(this.tle, Date.now())
     })
 
